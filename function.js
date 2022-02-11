2541d7ebaeb26b59d14febf6f1f1216ade088bf4
andyerdene <97221043+andyerdene@users.noreply.github.com>
2022-02-11 16:11:47 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+let raining = true;
+let snowing = false;
+while (raining || snowing) {
+  console.log("doing something here");
+}
+length;
+/*
+*loop eer hereglegchees 10n ner avna promt() commandaar
+avj baigaa nersee array ruu nemj oruulnaa push('ner')
+*buh nersiig loop davltaar nerniih n urt ni 5-s ih buh nersiig string dotor tsugluulaad 
+daraa n hevlii
+ */
